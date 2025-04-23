e1571ed570224b5d46fc84bd2436febaf1941768
blissed-out <none@none.local>
2025-04-23 07:17:03 +0545

    removed message content + fixed super error

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -1,11 +1,6 @@
 class ApiError extends Error {
-  constructor(
-    statusCode,
-    message = "something went wrong",
-    errors = [],
-    stack = "",
-  ) {
-    Super(message);
+  constructor(statusCode, message, errors = [], stack = "") {
+    super(message);
     this.statusCode = statusCode;
     this.message = message;
     this.success = false;
