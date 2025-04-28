329fe9691cadcaa349d444a372531ade7b02c81a
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-28 22:52:44 +0545

    feat: add cors for security

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import auth from "./routes/auth.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: [process.env.HOST],
+        methods: ["GET", "PUT", "DELETE", "POST"],
+    }),
+);
 
 app.use("/api/v1/users", auth);
 
