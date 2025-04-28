a55e11a79aa2232763991c79d65490de01993b19
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-28 22:55:46 +0545

    feat: add cookie middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import auth from "./routes/auth.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(
         methods: ["GET", "PUT", "DELETE", "POST"],
     }),
 );
+
+app.use(cookieParser());
 
 app.use("/api/v1/users", auth);
 
