e89f0b93ee10f0e8083fdd6aaf4b02860ec389b0
blissed-out <aadarshbanjade.computer@gmail.com>
2025-04-30 16:14:31 +0545

    fix: correct response message

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -2,12 +2,19 @@ import jwt from "jsonwebtoken";
 export const isLoggedIn = async (req, res, next) => {
     const token = req.cookies.token;
 
+    if (!token) {
+        return res.status(200).json({
+            success: false,
+            message: "cookie not found",
+        });
+    }
+
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     if (!decoded)
         return res.status(401).json({
             success: false,
-            message: "Session expired",
+            message: "Invalid cookie",
         });
 
     next();
