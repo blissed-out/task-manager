77c4c485f54d9ed85a458fb4b21013e62b9c9f04
blissed-out <aadarshbanjade.computer@gmail.com>
2025-05-02 14:57:59 +0545

    add: reigstration validation logic

---
+++ src/validator/index.js
@@ -0,0 +1,28 @@
+import { body, validationResult } from "express-validator";
+
+const registrationValidation = () => {
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("email field is required")
+        .isEmail()
+        .withMessage("Not valid email");
+
+    body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("username field is require")
+        .isLength({ min: 3 })
+        .withMessage("username must be atleast 3 characters")
+        .isLength({ max: 12 })
+        .withMessage("username must not be more than 12 characters");
+
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("password field is required")
+        .isLength({ min: 6 })
+        .withMessage("password must be atleast 6 characters");
+};
+
+export { registrationValidation };
